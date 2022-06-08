3847649d540b9051bdac88d650e0e1db3bee032a
Jori Jaatinen <jori@jaatinen.id.au>
2022-06-08 14:18:00 +1000

    Test commit

--- Source/DashboardTabApp/src/environments/environment.prod.ts
+++ Source/DashboardTabApp/src/environments/environment.prod.ts
@@ -12,7 +12,7 @@ export const environment = {
         extraQueryParameters: "",
         popUp: true,
         popUpUri: "/app-silent-start",
-        popUpWidth: 600,
+        popUpWidth: 601,
         popUpHeight: 535
     },
 
